483005ae39e622bd110f2cc9a70fa081c43cd693
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-16 12:57:44 +0200

    Reduce unique elements example

--- reduce.js
+++ reduce.js
@@ -1,5 +1,13 @@
-let a = [1, 2, 3, 4, 5]; // 15
+let a = [1, 2, 3, 4, 5, 2, 4, 1, 6, 7, 7, 5];
 
-let sum = a.reduce((sum, el) => sum + el, 0);
+//let sum = a.reduce((sum, el) => sum + el, 0);
+let b = a.reduce((acc, el, index, arr) => {
+  if (!arr.slice(0, index).includes(el) && (!arr.slice(index + 1).includes(el) || index == arr.length - 1)) {
+    acc.push(el);
+  }
 
-console.log(sum);
+  return acc;
+}, []);
+
+console.log(a);
+console.log(b);
